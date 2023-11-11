4f1868438007e62b58229bab0694a447c8643e00
SporeCloud <44228349+SporeCloud@users.noreply.github.com>
2023-11-11 13:00:31 +0000

    Delete algorithms/Roman to Integer/python3 directory
    typo oops

--- algorithms/Roman to Integer/python3/solution 2 (the cursed one).py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        roman_map = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-        s = s.replace("IV","IIII").replace("IX","IIIIIIIII").replace("XL","XXXX").replace("XC","XXXXXXXXX").replace("CD","CCCC").replace("CM","CCCCCCCCC")
-        num = 0
-        for char in s:
-            num+=roman_map[char]
-        return num
